a33fc7fcde276add23fa6570a8b4fc24ab1bf800
viettungvuong <viettungvuong@gmail.com>
2026-08-22 17:17:23 +0800

    Add: hand-of-straights - submission-6

---
+++ Data Structures & Algorithms/hand-of-straights/submission-6.py
@@ -0,0 +1,21 @@
+from collections import Counter
+from typing import List
+
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0:
+            return False
+
+        count = Counter(hand)
+
+        # Iterate over unique sorted cards
+        for card in sorted(count):
+            if count[card] > 0:
+                needed = count[card]
+                # Consume `needed` amount for all consecutive cards in the group (starting from the index)
+                for next_card in range(card, card + groupSize):
+                    if count[next_card] < needed:
+                        return False
+                    count[next_card] -= needed
+
+        return True
